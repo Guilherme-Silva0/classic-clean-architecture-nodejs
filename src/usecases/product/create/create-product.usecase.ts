3fde992dd8f226a8fa915c5f1244f4681d702873
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-05-10 12:36:08 -0300

    feat: :sparkles: Create product use case

---
+++ src/usecases/product/create/create-product.usecase.ts
@@ -0,0 +1,35 @@
+import { Product } from "../../../domain/product/entity/product";
+import { ProductGateway } from "./../../../domain/product/gateway/product.gateway";
+import { UseCase } from "./../../useCase";
+export type CreateProductInputDto = {
+  name: string;
+  price: number;
+};
+export type CreateProductOutputDto = {
+  id: string;
+};
+
+export class CreateProductUseCase
+  implements UseCase<CreateProductInputDto, CreateProductOutputDto>
+{
+  private constructor(private readonly productGateway: ProductGateway) {}
+
+  public static create(productGateway: ProductGateway) {
+    return new this(productGateway);
+  }
+
+  public async execute({
+    name,
+    price,
+  }: CreateProductInputDto): Promise<CreateProductOutputDto> {
+    const aProduct = Product.create(name, price);
+
+    await this.productGateway.save(aProduct);
+
+    const output: CreateProductOutputDto = {
+      id: aProduct.id,
+    };
+
+    return output;
+  }
+}
