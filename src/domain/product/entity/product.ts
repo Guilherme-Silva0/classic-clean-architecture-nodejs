2efee1c0e62d7bb4dadfeb1b94ae8e5090cdd302
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-05-10 18:09:21 -0300

    fix: :bug: Fix call function

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -10,7 +10,7 @@ export class Product {
 
   public static create(name: string, price: number) {
     return new this({
-      id: crypto.randomUUID.toString(),
+      id: crypto.randomUUID().toString(),
       name,
       price,
       quantity: 0,
