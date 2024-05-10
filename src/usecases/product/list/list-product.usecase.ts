1e93f97f43234d5560ed29923ef64cbff2c90248
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-05-10 13:01:39 -0300

    feat: :sparkles: Create list products use case

---
+++ src/usecases/product/list/list-product.usecase.ts
@@ -0,0 +1,41 @@
+import { Product } from "../../../domain/product/entity/product";
+import { ProductGateway } from "../../../domain/product/gateway/product.gateway";
+import { UseCase } from "../../useCase";
+
+export type ListProductInputDto = void;
+
+export type ListProductOutputDto = {
+  products: {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+  }[];
+};
+
+export class ListProductUseCase
+  implements UseCase<ListProductInputDto, ListProductOutputDto>
+{
+  private constructor(private readonly productGateway: ProductGateway) {}
+
+  public static create(productGateway: ProductGateway) {
+    return new this(productGateway);
+  }
+
+  public async execute(): Promise<ListProductOutputDto> {
+    const aProducts = await this.productGateway.list();
+
+    return this.presentOutput(aProducts);
+  }
+
+  private presentOutput(products: Product[]): ListProductOutputDto {
+    return {
+      products: products.map((p) => ({
+        id: p.id,
+        name: p.name,
+        price: p.price,
+        quantity: p.quantity,
+      })),
+    };
+  }
+}
